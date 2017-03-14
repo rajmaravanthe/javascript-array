f96e8db3c0f57b750e05e0a932ae9f2241e38c8c
rajesha maravanthe <rraajjpoojary@gmail.com>
2017-03-14 10:58:17 +0530

    added new files

---
+++ app.js
@@ -0,0 +1,59 @@
+var employees = [
+    {
+        name: "Rajesha Maravanthe",
+        age: "28",
+        salary: "1500000 per annum",
+        address: {
+            city: "Kundapura",
+            state: "Karnataka",
+            pin: "576224"
+        }
+    },
+    {
+        name: "Mahesh Maravanthe",
+        age: "23",
+        salary: "150000 per annum",
+        address: {
+            city: "Kundapura",
+            state: "Karnataka",
+            pin: "576222"
+        }
+    },
+    {
+        name: "Santhosh Maravanthe",
+        age: "26",
+        salary: "3500000 per annum",
+        address: {
+            city: "Udupi",
+            state: "Karnataka",
+            pin: "573224"
+        }
+    },
+    {
+        name: "Sai Baba ",
+        age: "58",
+        salary: "3000000 per annum",
+        address: {
+            city: "Maravanthe",
+            state: "Karnataka",
+            pin: "576124"
+        }
+    },
+    {
+        name: "Ganesh Ekadanta",
+        age: "25",
+        salary: "5000000 per annum",
+        address: {
+            city: "Mangalore",
+            state: "Karnataka",
+            pin: "571224"
+        }
+    }
+];
+
+console.log("Name: "+ employees[3].name);
+console.log("Age: "+ employees[3].age);
+console.log("Salary: "+ employees[3].salary);
+console.log("City: "+ employees[3].address.city);
+console.log("State: "+ employees[3].address.state);
+console.log("Pin: "+ employees[3].address.pin);
